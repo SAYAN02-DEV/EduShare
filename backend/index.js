bd0cec6856e6ac84f83c8cf08532c9d6c818364c
SAYAN02-DEV <sayan2003.dev@gmail.com>
2025-07-08 01:59:12 +0530

    fixed frontend url

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'https://edu-share-frontend.vercel.app/',
+  origin: 'https://edu-share-frontend.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'token'],
   credentials: false,
